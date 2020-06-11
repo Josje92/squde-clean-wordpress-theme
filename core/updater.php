9538ce7123ced098f8d7209af321e1195bd3ea80
Joshua Weidema <joshua@squde.nl>
2020-06-11 16:04:20 +0200

    Bugfixes

--- core/updater.php
+++ core/updater.php
@@ -3,8 +3,8 @@ require_once 'include/updater/updater.php';
 
 add_action('init', function() {
     if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-        $config = array(
-            'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        $config = [
+            'slug' => '../themes/squde-clean-wordpress-theme', // this is the slug of your plugin
             'proper_folder_name' => 'squde-clean-wordpress-theme', // this is the name of the folder your plugin lives in
             'api_url' => 'https://api.github.com/repos/Josje92/squde-clean-wordpress-theme', // the GitHub API url of your GitHub repo
             'raw_url' => 'https://raw.github.com/Josje92/squde-clean-wordpress-theme/master', // the GitHub raw url of your GitHub repo
@@ -15,7 +15,7 @@ add_action('init', function() {
             'tested' => '5.4', // which version of WordPress is your plugin tested up to?
             'readme' => 'README.md', // which file to use as the readme for the version number
             'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
-        );
+        ];
         new WP_GitHub_Updater($config);
     }
 });
