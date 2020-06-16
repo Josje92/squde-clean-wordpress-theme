d71714739f19785f3f7a4c87eb7f9812adca18e4
Joshua Weidema <joshua@squde.nl>
2020-06-16 09:31:31 +0200

    Supports menus now

--- functions.php
+++ functions.php
@@ -26,6 +26,12 @@ function twentytwenty_theme_support()
 
 
     /*
+     * Add menu support
+     */
+    add_theme_support( 'menus' );
+
+
+    /*
      * Let WordPress manage the document title.
      * By adding theme support, we declare that this theme does not use a
      * hard-coded <title> tag in the document head, and expect WordPress to
